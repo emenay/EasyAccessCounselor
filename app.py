14d87dd7e9b445dfa43af7b5e265bc8d445c4960
lin-zhou <zhou.lindsay@yahoo.com>
2020-02-18 01:29:19 -0500

    Created templates, added CSS links

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
+
+@app.route('/college_list')
+def college_list():
+    return render_template('college_list.html')
+
+@app.route('/notes')
+def notes():
+    return render_template('notes.html')
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
