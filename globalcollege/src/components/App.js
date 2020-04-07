08332b8e042884db85794c98102ffd5b1d61be28
justinmmott <justinmmott@gmail.com>
2020-04-07 05:34:18 -0400

    changed imports to match formating

--- globalcollege/src/components/App.js
+++ globalcollege/src/components/App.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
-
-import '../css/App.css';
-import Login from './LoginPage';
-import Home from './HomePage';
-import About from './AboutPage';
-import { UserContext } from "../providers/UserProvider";
 import { 
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+
+import '../css/App.css';
+import Login from './LoginPage';
+import Home from './HomePage';
+import About from './AboutPage';
+import { UserContext } from "../providers/UserProvider";
+
 
 export default function App() {
   const user = useContext(UserContext);
