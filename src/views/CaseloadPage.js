abac26e3e31bfa12d74901a95fea0e702f2960de
Komal <kessara0@live.unc.edu>
2020-04-16 19:38:55 -0400

    added spreadsheet view

--- src/views/CaseloadPage.js
+++ src/views/CaseloadPage.js
@@ -1,5 +1,36 @@
 import React from 'react';
 import { db } from '../firebase/firebase';
+import Spreadsheet from "react-spreadsheet";
+
+const RangeView = ({ cell, getValue }) => (
+  <input
+    type="range"
+    value={getValue({ data: cell })}
+    disabled
+    style={{ pointerEvents: "none" }}
+  />
+);
+ 
+const RangeEdit = ({ getValue, cell, onChange }) => (
+  <input
+    type="range"
+    onChange={e => {
+      onChange({ ...cell, value: e.target.value });
+    }}
+    value={getValue({ data: cell }) || 0}
+    autoFocus
+  />
+);
+ 
+const data = [
+  [{ value: "Flavors" }],
+  [({ value: "Vanilla" }, { value: "Chocolate" })],
+  [{ value: "Strawberry" }, { value: "Cookies" }],
+  [
+    { value: "How much do you like ice cream?" },
+    { value: 100, DataViewer: RangeView, DataEditor: RangeEdit }
+  ]
+];
 
 class CaseloadPage extends React.Component {
   state = {
@@ -14,7 +45,7 @@ class CaseloadPage extends React.Component {
     render() {
         return (
           <div>
-            This is the Caseload Management page.
+            <Spreadsheet data={data} />
           </div>
         );
     }
