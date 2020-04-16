f62b201e168e7415b2a4adb4b727f1e5699061df
rupali saini <43911326+rupalisaini@users.noreply.github.com>
2020-04-16 19:00:23 -0400

    added database to caseload

--- src/views/CaseloadPage.js
+++ src/views/CaseloadPage.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { db } from '../firebase/firebase';
 
 class CaseloadPage extends React.Component {
+  state = {
+      data : null,
+  }
+
+  async componentDidMount() {
+      let doc = await db.collection("NeedMet").doc("Adrian College").get();
+      this.setState({data: doc.data()})
+  }
+  
     render() {
         return (
           <div>
