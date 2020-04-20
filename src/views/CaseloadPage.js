8add55f6713dae458a31203f7fd62c7ec1454b03
rupali saini <43911326+rupalisaini@users.noreply.github.com>
2020-04-20 14:49:39 -0400

    converted grid to react

--- src/views/CaseloadPage.js
+++ src/views/CaseloadPage.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { db } from '../firebase/firebase';
+import * as data from '../data_caseload_management.json';
+import '../css/CaseloadPage.css'
+import 'bulma/css/bulma.css'
 
 class CaseloadPage extends React.Component {
-  state = {
-      data : null,
-  }
-
-  async componentDidMount() {
-      let doc = await db.collection("NeedMet").doc("Adrian College").get();
-      this.setState({data: doc.data()})
-  }
-  
     render() {
+        console.log(data[2]);
+        var arr = [];
+        Object.keys(data).forEach(function(key) {
+            arr.push(data[key]);
+            console.log(data[key].Name)
+        });
+        console.log(arr)
         return (
-          <div>
-            This is the Caseload Management page.
-          </div>
-        );
+            <div id="root">
+                <div id="grid">
+                {arr[0].map(person => <div key={person.id} className="square">
+                    <div className="card">
+                        <div className="card-content">
+                            <p className="title is-4">{person.Name}</p>
+                            <p className="subtitle is-6">{person.Essay}</p>
+                            <div className="content">
+                                <p>{person["Early Apps"]}</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>)}
+                </div>
+            </div>
+        )
     }
+
 }
 
 export default CaseloadPage;
