9cae2b7c91301d0f2d8948cfbacd1fda2855b7cc
lin-zhou <zhou.lindsay@yahoo.com>
2020-04-29 18:14:51 -0400

    New student doc --> create a copy under corresponding counselor

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,39 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+/**
+ * Makes a copy of a student's document under their corresponding counselor
+ *
+ * @param {Object} change Contains the before and after state of a student's doc.
+ * @param {Object} context Details about the event.
+ */
+
+const copyProfile = (snap, context) => {
+  const { firstName, lastName, uid, cuid } = snap.data();
+
+  if (cuid) {
+    return db
+      .collection('counselors')
+      .doc(cuid)
+      .collection('students')
+      .doc(uid)
+      .set({
+          firstName,
+          lastName,
+          uid
+      })
+      .catch(console.error);
+  } else {
+    return;
+  }
+};
+
+// TODO: functions for onDelete and onUpdate
+
+module.exports = {
+  copyOnWrite: functions.firestore.document('students/{studentId}').onCreate(copyProfile)
+};
