c61b6b7f054ec0b40e44308c8d1f8dad450b822a
jake612 <jake612@live.unc.edu>
2020-06-26 09:05:00 -0400

    Added the ability to reverse sort and set the framework for searching by name

--- src/views/StudentProfilesPage.js
+++ src/views/StudentProfilesPage.js
@@ -25,6 +25,7 @@ class StudentProfilesPage extends React.Component{
         this.state = {
             selectedCard: null,
             sortField: "Testing",
+            sortReverse: false,
             searchString: "",
             flagMap: flagMap,
             flagToggle: false
@@ -35,15 +36,24 @@ class StudentProfilesPage extends React.Component{
     compare = (a, b) => {
         let comparison = 0;
         if (b[this.state.sortField] > a[this.state.sortField]) {
-          comparison = 1;
-        } else if (b[this.state.sortField] < a[this.state.sortField]) {
           comparison = -1;
+        } else if (b[this.state.sortField] < a[this.state.sortField]) {
+          comparison = 1;
         }
+        if (this.state.sortReverse) comparison *= -1;
         return comparison;
     }
 
     changeSort = (field) => {
         this.setState({sortField: field});
+    }
+
+    changeSearchString = (string) => {
+        this.setState({searchString: string});
+    }
+
+    reverseSortOrder = () => {
+        this.setState({sortReverse: !this.state.sortReverse});
     }
 
     clickCard = (name) => {
@@ -64,10 +74,16 @@ class StudentProfilesPage extends React.Component{
 
     render(){
         let data = this.state.flagToggle ? data2.filter(person => {return this.state.flagMap.get(person.id)}) : data2;
+        if (this.state.searchString !== "") {
+            data = data.filter(person=>{
+                return this.state.searchString === person.Name.toLowerCase().slice(0, this.state.searchString.length);
+            });
+        }
         return <div className="profiles-content">
             <div className="profiles-header">
                 <button className="flag-toggle" onClick={this.flagToggle}>Toggle Flags</button>
                 <DropdownSortMenu changeSort={this.changeSort} />
+                <button className="flag-toggle" onClick={this.reverseSortOrder}>Reverse Sort</button>
             </div>
             {this.state.selectedCard && <StudentDetailsModal exitModal={this.exitModal} studentName={this.state.selectedCard} />}
             <GridView data={data.sort(this.compare)} clickCard={this.clickCard} clickFlag={this.clickFlag} flags={this.state.flagMap}/>
