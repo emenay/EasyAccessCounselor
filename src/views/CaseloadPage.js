7e3a0995484e2d3d548358c38ce3b623a2b32740
jake612 <jake612@live.unc.edu>
2020-07-03 17:50:54 -0400

    Implemented the first version of the caseload management table

--- src/views/CaseloadPage.js
+++ src/views/CaseloadPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import MainDataView from '../components/dataViews/MainDataView';
-import 'bulma/css/bulma.css'
+import ReactDataGrid from 'react-data-grid';
+import data2 from "../data_caseload_management.json";
+import 'react-data-grid/dist/react-data-grid.css';
 
 class CaseloadPage extends React.Component {
   constructor(props){
@@ -10,27 +11,17 @@ class CaseloadPage extends React.Component {
       searchString: ""
     });
     this.fields = [
-      "Goal",
-      "Meetings",
-      "Tasks",
-      "Visit",
-      "Testing",
-      "List",
-      "Fee Waiver",
-      "Early Apps",
-      "Essay",
-      "Teacher Recs",
-      "Counselor Rec",
-      "Resume",
-      "FAFSA",
-      "CSS Profile",
-      "Results",
-      "Verification",
-      "Award Letters",
-      "Appeal",
-      "Spring Visit",
-      "Decision",
-      "Latest"
+      {key: "uid", name: "ID", editable: false},
+      {key: 'Goal', name: 'Goal', editable: true},
+      {key: "Meetings", name: 'Meetings', editable: true},
+      {key: "Tasks", name: 'Tasks', editable: true},
+      {key: "Visit", name: 'Visit', editable: true},
+      {key: "Testing", name: 'Testing', editable: true},
+      {key: "List", name: 'List', editable: true},
+      {key: "Fee Waiver", name: 'Fee Waiver', editable: true},
+      {key: "Early Apps", name: 'Early Apps', editable: true},
+      {key: "Essay", name: 'Essay', editable: true},
+      {key: "Teacher Recs", name: 'Teacher Recs', editable: true}
     ];
     
   }
@@ -39,12 +30,18 @@ class CaseloadPage extends React.Component {
     console.log(e.target.value);
   }
   render(){
-    console.log("rendered");
     return (
       <div className="caseload-content">
         <div className="profiles-header">
           <input type="text" id="myInput" onKeyUp={this.changeSearchString} placeholder="Search for Students.." />
         </div>
+                  
+          <ReactDataGrid
+            columns={this.fields}
+            rows={data2}
+            rowGetter={i => data2[i]}
+            rowsCount={data2.length}
+            minHeight={2} />
       </div>
     );
   }
