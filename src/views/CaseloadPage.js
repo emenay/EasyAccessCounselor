8addb3fa51c7d9accbec4bc045e5232790a93fc1
jake612 <jake612@live.unc.edu>
2020-07-04 12:50:37 -0400

    Implemented the first version of the caseloadmanagement table with custom styling, json data, and searchbar for table. Need to add actually editing rows and adding people

--- src/views/CaseloadPage.js
+++ src/views/CaseloadPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import ReactDataGrid from 'react-data-grid';
 import data2 from "../data_caseload_management.json";
-import 'react-data-grid/dist/react-data-grid.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import '../css/CaseloadPage.css';
 
 class CaseloadPage extends React.Component {
   constructor(props){
@@ -11,37 +13,55 @@ class CaseloadPage extends React.Component {
       searchString: ""
     });
     this.fields = [
-      {key: "uid", name: "ID", editable: false},
-      {key: 'Goal', name: 'Goal', editable: true},
-      {key: "Meetings", name: 'Meetings', editable: true},
-      {key: "Tasks", name: 'Tasks', editable: true},
-      {key: "Visit", name: 'Visit', editable: true},
-      {key: "Testing", name: 'Testing', editable: true},
-      {key: "List", name: 'List', editable: true},
-      {key: "Fee Waiver", name: 'Fee Waiver', editable: true},
-      {key: "Early Apps", name: 'Early Apps', editable: true},
-      {key: "Essay", name: 'Essay', editable: true},
-      {key: "Teacher Recs", name: 'Teacher Recs', editable: true}
+      {field: "id", headerName: "ID", width: "70"},
+      {field: "Name", headerName: "Name", sortable: true, filter: true, editable: true, resizable: true},
+      {field: 'Goal', headerName: 'Goal', sortable: true, filter: true, editable: true, resizable: true},
+      {field: "Meetings", headerName: 'Meetings', sortable: true, editable: true, filter: 'agNumberColumnFilter', resizable: true},
+      {field: "Tasks", headerName: 'Tasks', sortable: true, editable: true, filter: 'agNumberColumnFilter', resizable: true},
+      {field: "Visit", headerName: 'Visit', sortable: true, filter: true, editable: true, resizable: true},
+      {field: "Testing", headerName: 'Testing', sortable: true, editable: true, filter: 'agNumberColumnFilter', resizable: true},
+      {field: "List", headerName: 'List', sortable: true, filter: true, editable: true, resizable: true},
+      {field: "Fee Waiver", headerName: 'Fee Waiver', sortable: true, filter: true, editable: true, resizable: true},
+      {field: "Early Apps", headerName: 'Early Apps', sortable: true, filter: true, editable: true, resizable: true},
+      {field: "Essay", headerName: 'Essay', sortable: true, filter: true, editable: true, resizable: true},
+      {field: "Teacher Recs", headerName: 'Teacher Recs', sortable: true, filter: true, editable: true, resizable: true}
     ];
     
   }
 
-  getContent = (e) => {
-    console.log(e.target.value);
+  componentDidMount() {
+    console.log("mounted");
+    // TODO: where fetch data for display will be implemented
   }
+
+  async cellEditingStopped(e) {
+    console.log(e.colDef.field + ": " + e.data.id + " " + e.data[e.colDef.field]);
+    // TODO: where updating the database will occur
+  }
+
+  changeSearchString = (e) => {
+    this.setState({searchString: e.target.value});
+  }
+
   render(){
     return (
       <div className="caseload-content">
         <div className="profiles-header">
-          <input type="text" id="myInput" onKeyUp={this.changeSearchString} placeholder="Search for Students.." />
+          <input type="text" id="myInput" onKeyUp={this.changeSearchString} placeholder="Search Table..." />
         </div>
                   
-          <ReactDataGrid
-            columns={this.fields}
-            rows={data2}
-            rowGetter={i => data2[i]}
-            rowsCount={data2.length}
-            minHeight={2} />
+        <div
+          className="ag-theme-alpine"
+          style={{
+          height: '600px',
+          width: '100%' }}>
+          <AgGridReact
+            quickFilterText={this.state.searchString}
+            onCellEditingStopped={(e) => this.cellEditingStopped(e)}
+            columnDefs={this.fields}
+            rowData={data2}>
+          </AgGridReact>
+        </div>
       </div>
     );
   }
