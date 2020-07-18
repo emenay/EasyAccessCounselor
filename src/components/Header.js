053a4865899987fd5b779cade0d9e0e767187717
jake612 <jake612@live.unc.edu>
2020-07-18 07:50:09 -0400

    Changes to selecting cohort
    Querying for cohorts done on an array in the counselor fields called cohorts that contains cohort ids
    Added a No Cohorts message for dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,15 +12,28 @@ class Header extends React.Component{
 		this.setState({selectedCohort: e.target.value});
 	}
 
+	displayHelper = () => {
+		if (this.context.state.cohorts.length === 0) {
+			return(
+				<select className="header-select" value={"No Cohorts"} onChange={this.changeSelected}>
+					<option className="header-option" value="No Cohorts" hidden="true">No Cohorts</option>
+				</select>);
+
+		} else {
+			return(
+			<select className="header-select" value={this.context.state.selectedCohort ? this.context.state.selectedCohort : ""} onChange={this.changeSelected}>
+			{this.context.state.cohorts.map((cohort, index)=>{
+				return <option key={index} className="header-option" value={cohort.id}>{cohort.name}</option>
+			})}
+			</select>);
+		}
+	}
+
 	render() {
 		if (this.props.isLoggedIn) {
 			return (
 			<div className="header">
-			<select className="header-select" value={this.context.state.selectedCohort ? this.context.state.selectedCohort : ""} onChange={this.changeSelected}>
-				{this.context.state.cohorts.map((cohort, index)=>{
-					return <option key={index} className="header-option" value={cohort.id}>{cohort.name}</option>
-				})}
-			</select>
+			{this.displayHelper()}
 			<SignOutBtn/>
 		</div>);
 		}
--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -18,7 +18,6 @@ class UserProvider extends Component {
                 return querySnapshot.data().cohorts;
                 })
             .then(cohorts=>{
-                console.log(cohorts);
                 db.collection("student_counselors").where(firebase.firestore.FieldPath.documentId(), "in", cohorts).get()
                 .then(querySnapshot=>{
                     return querySnapshot.docs.map(doc =>{
@@ -31,8 +30,10 @@ class UserProvider extends Component {
                     user: user,
                     cohorts: data,
                     selectedCohort: this.state.selectedCohort ? this.state.selectedCohort : data.length > 0 ?  data[0].id : null
-                  }));
-            });
+                  }))
+                  .catch(error=>{console.log(error)});
+            })
+            .catch(error=>{console.log(error)});
           } else {
               this.setState({
                 user: null,
