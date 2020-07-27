0ef441a4acb2279d26c95f70d31331589e01eb97
jake612 <jake612@live.unc.edu>
2020-07-27 17:31:28 -0400

    Further changes to make design closer to that of sema design

--- src/components/SignOutBtn.js
+++ src/components/SignOutBtn.js
@@ -3,13 +3,9 @@ import '../css/components.css';
 
 import { signOut } from '../firebase/firebase';
 
-import Button from '@material-ui/core/Button';
-
 function SignOut() {
   return (
-    <Button style={{width: "100px"}} onClick={signOut}>
-      Sign Out
-    </Button>
+    <button className='signout' onClick={signOut} />
   )
 }
 
--- src/views/App.js
+++ src/views/App.js
@@ -11,14 +11,9 @@ import { createBrowserHistory } from 'history';
 
 import '../css/App.css';
 import Login from './LoginPage';
-import HomeDefault from './HomePageDefault';
-import Dashboard from './Dashboard';
-import Account from './AccountPage';
 import Caseload from './CaseloadPage';
-import CollegeList from './CollegeListPage';
 import Settings from './SettingsPage';
 import CohortCreation from './CohortCreation';
-import Notes from './NotesPage';
 import StudentProfilesPage from './StudentProfilesPage';
 import Header from '../components/Header';
 import Sidenav from '../components/Sidenav';
@@ -31,31 +26,27 @@ export default function App() {
   return (
     <Router history={history}>
       <div className="page-container">
-        <Sidenav isLoggedIn={isLoggedIn}/>
+      <Header isLoggedIn={isLoggedIn}/>
+        
         <div className="page-content">
+        <Sidenav isLoggedIn={isLoggedIn}/>
           <Switch>
             <Route path="/profiles">
-              <Header isLoggedIn={isLoggedIn}/>
               { user.state.user ? <StudentProfilesPage /> : <Login /> }
             </Route>
             <Route path="/caseload_management">
-              <Header isLoggedIn={isLoggedIn}/>
               { user.state.user ? <Caseload cuid={user.uid}/> : <Login /> }
             </Route>
             <Route path="/settings">
-              <Header isLoggedIn={isLoggedIn}/>
               { user.state.user ? <Settings currentUser={user} /> : <Login /> }
             </Route>
             <Route path="/cohortcreation">
-              <Header isLoggedIn={isLoggedIn}/>
               { user.state.user ? <CohortCreation /> : <Login /> }
             </Route>
             <Route path="/cohortcreation2">
-              <Header isLoggedIn={isLoggedIn}/>
               { user.state.user ? <CohortCreation /> : <Login /> }
             </Route>
             <Route path="/">
-              <Header isLoggedIn={isLoggedIn}/>
               { user.state.user ? <StudentProfilesPage /> : <Login /> }
             </Route>
           </Switch>
