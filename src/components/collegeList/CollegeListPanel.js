af2bd095a9670759a07336ae22ae76e050567262
leohuang990 <haopenghuang0@gmail.com>
2021-11-07 23:21:20 -0500

    update college list

--- src/components/collegeList/CollegeListPanel.js
+++ src/components/collegeList/CollegeListPanel.js
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Switch from '@mui/material/Switch';
 
 import { Table } from 'reactstrap';
+import Classification_table from './Classification_table';
+import Search_college from './Search_college';
 
 export default function CollegeListPanel(props) {
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
     let affordabilityInfo = ["GPA", "Class Rank", "SAT", "ACT", "EFC", "Ability to Pay"];
     let fitInfo = ["Major 1", "Major 2", "Distance from Home", "Region", "College Size", "College Diversity", "College Type", "Religion", "Military/ROTC", "Athletics"];
-    let allinfo = ["GPA", "Class Rank", "SAT", "ACT", "EFC", "Ability to Pay","Major 1", "Major 2", "Distance from Home", "Region", "College Size", "College Diversity", "College Type", "Religion", "Military/ROTC", "Athletics"];
+    let to_delete=[2.5, '1 out of 200', 1000, 23, 2040, "Mismatch", 27701]
+    let to_delete2=[2.5, 1, 1000, 23, 2040, "Mismatch", 27701]
     var dbinfo = ["gpa", "classRank", "sat", "act", "efc", "payMismatch"];
     var dbinfo2 = ["major1", "major2", "distancefromHome", "region", "collegeSize", "collegeDiversity", "collegeType", "religion", "rotc", "athletics"];
 
@@ -16,32 +19,46 @@ export default function CollegeListPanel(props) {
         <Table>
         <thead>
           <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>check</th>
+            <th>Information</th>
+            <th>Counselor</th>
+            <th><button>sync</button></th>
+            <th>Student</th>
+          </tr>
+          <br/>
+          <br/>
+          <tr>
+            <th>Affordabiity&Selectivity</th>
+            <th></th>
+            <th><button>check all</button></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@<Switch {...label} defaultChecked /></td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@<Switch {...label} defaultChecked /></td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
+          {affordabilityInfo.map((ele, idx)=>(<tr>
+            <th scope="row">{ele}</th>
+            <td>{to_delete[idx]}</td>
             <td><Switch {...label} defaultChecked /></td>
-          </tr>
+            <td>{to_delete2[idx]}</td>
+          </tr>))}
+        </tbody>
+        <thead><br/><br/>
+        <tr>
+            <th>Fit Information</th>
+            <th></th>
+            <th><button>check all</button></th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+          {fitInfo.map(ele=>(<tr>
+            <th scope="row">{ele}</th>
+            <td>to fill</td>
+            <td><Switch {...label} defaultChecked /></td>
+            <td>to fill</td>
+          </tr>))}
         </tbody>
       </Table>
+      <Classification_table/>
+      <Search_college/>
     </div>);
 }
